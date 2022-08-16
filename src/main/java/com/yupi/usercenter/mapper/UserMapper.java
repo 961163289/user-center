d50d7e6ab74ba5905ff85284c33484fb753c75dd
zi.peng <44833128+961163289@users.noreply.github.com>
2022-08-16 23:40:48 +0800

    实现注册功能

---
+++ src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.usercenter.mapper;
+
+import com.yupi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author win11
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2022-08-16 20:42:49
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
