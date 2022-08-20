c165b1d39d4538a66dd3bd7fd57b937b4fe99bd4
zi.peng <44833128+961163289@users.noreply.github.com>
2022-08-20 15:20:52 +0800

    自定义错误码,返回类支持返回正常和错误

--- src/main/java/com/yupi/usercenter/common/ResultUtils.java
+++ src/main/java/com/yupi/usercenter/common/ResultUtils.java
@@ -7,8 +7,26 @@ package com.yupi.usercenter.common;
  */
 public class ResultUtils {
 
+    /**
+     * 成功
+     *
+     * @param data
+     * @param <T>
+     * @return
+     */
     public static <T> BaseResponse<T> success(T data) {
         return new BaseResponse<>(0, data, "ok");
+    }
+
+    /**
+     * 失败
+     *
+     * @param errorCode
+     * @return
+     * @param <T>
+     */
+    public static <T> BaseResponse<T> error(ErrorCode errorCode) {
+        return new BaseResponse<>(errorCode);
     }
 
 }
